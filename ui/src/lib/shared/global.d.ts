327c7119d835259e5b1e835ed5933a2d3342c3b6
jncoble <coble.jesse@gmail.com>
2022-06-10 23:03:22 -0400

    Committing before doing more changes to the svelte config file

--- ui/src/lib/shared/global.d.ts
+++ ui/src/lib/shared/global.d.ts
@@ -1,3 +1,16 @@
+import {ResponseStatus} from "./dtos";
+
 declare const Checkbox: typeof import('$elements/Checkbox.svelte')['default']
 declare const ErrorSummary: typeof import('$elements/ErrorSummary.svelte')['default']
 declare const Input: typeof import('$elements/Input.svelte')['default']
+
+export type User = {
+    userId: string;
+    sessionId: string;
+    userName: string;
+    displayName: string;
+    referrerUrl: string;
+    profileUrl: string;
+    roles: string[];
+    permissions: string[];
+}
--- ui/src/lib/shared/index.ts
+++ ui/src/lib/shared/index.ts
@@ -1,33 +1,75 @@
 // import Script from 'svelte/types/compiler/interfaces';
+import Cookies from 'js-cookie'
+import {browser} from '$app/env'
+import {JsonServiceClient} from '@servicestack/client';
+import type {Writable} from 'svelte/store';
+import {writable} from 'svelte/store';
+import type {User} from './global'
 
-import { JsonServiceClient } from '@servicestack/client';
-import { writable } from 'svelte/store';
 // import {navigate} from 'svelte-routing';
 
-import { goto } from '$app/navigation';
+import {goto} from '$app/navigation';
 
-import { Authenticate, AuthenticateResponse } from './dtos';
+import {Authenticate, AuthenticateResponse} from './dtos';
+// import {logout, removeCookie} from "./auth";
 
-export var client = new JsonServiceClient('/');
+export var client = new JsonServiceClient('http://localhost:5000/');
 
-export let userSession = writable(null);
+export let userSession: Writable<AuthenticateResponse | null>;
+userSession = writable<AuthenticateResponse | null>(null);
 
 // @ts-ignore
-export const login = (response: AuthenticateResponse) => userSession.update((_) => response);
+export const login = async (response: AuthenticateResponse) => {
+    if (browser) {
+
+        let user: User = {
+            userId: response.userId,
+            sessionId: response.sessionId,
+            userName: response.userName,
+            displayName: response.displayName,
+            referrerUrl: response.referrerUrl,
+            profileUrl: response.profileUrl,
+            roles: response.roles,
+            permissions: response.permissions
+        }
+
+        setCookie('token', response.bearerToken, {expires: 1, httpOnly: true, sameSite: 'lax'})
+        await setCookie('user', user, {expires: 1, httpOnly: true, sameSite: 'lax'})
+    }
+    userSession.update((_) => response);
+}
 export const signout = async (redirect = true) => {
-	userSession.update((_) => null);
-	await client.post(new Authenticate({ provider: 'logout' }));
-	if (redirect) goto('/');
+    userSession.update((_) => null);
+    await client.post(new Authenticate({provider: 'logout'}));
+    await removeCookie('token')
+    await removeCookie('user')
+    if (redirect) await goto('/');
 };
 
-export const redirect = (path: string) => {
-	goto(path);
+export const redirect = async (path: string) => {
+    await goto(path);
 };
+
+export const setCookie = (key: any, value: any, opts: any) => {
+    Cookies.set(key, value, opts)
+}
+
+export const removeCookie = async (key: any) => {
+    if (browser) {
+        await Cookies.remove(key)
+    }
+}
 
 export const checkAuth = async () => {
-	try {
-		login(await client.post(new Authenticate()));
-	} catch (e) {
-		await signout(false);
-	}
+    try {
+        await login(await client.post(new Authenticate()));
+    } catch (e) {
+        await signout(false);
+    }
 };
+
+export const handleSession = async (res: any) => {
+    if (res.status === 440) {
+        await signout();
+    }
+}
